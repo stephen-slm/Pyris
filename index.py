a364d045c9f5f25e8bb8f85403df1a8936cb692c
Stephen <smmstephenmiller@gmail.com>
2016-12-28 12:10:27 +0000

    custom scraping
    * custom scraping for any subreddits now added with support of the ini file
    
    * error code now added which will later be referenced in github

--- index.py
+++ index.py
@@ -18,18 +18,28 @@ CONFIG.read('./settings.ini')
 #settings
 IMAGE_PATH = CONFIG.get('settings', 'images_path')
 IMAGE_LIMIT = CONFIG.getint('settings', 'image_limit')
-CUTE_REDDITS = str(CONFIG.get("settings", "cute_reddits")).replace(" ", "").split(",")
 
 #options
 BUILD_CORE = CONFIG.getboolean("options", "build_core")
-NAMES_TYPE = CONFIG.getboolean("options", "names_type")
+NAME_TYPE = CONFIG.get("options", "name_type")
+
+#scraping_options
+FOLDER_NAMES = str(CONFIG.get("scraping_options", "folder_names")).replace(" ", "").split(",")
+
 
 # accessing the reddit class and gathering the front page sub names
 if BUILD_CORE:
     REDDIT = Reddit()
-    CORE = Redditscraping(REDDIT.frontpage, (IMAGE_PATH + "/core/"), IMAGE_LIMIT, NAMES_TYPE)
+    CORE = Redditscraping(REDDIT.frontpage, (IMAGE_PATH + "/core/"), IMAGE_LIMIT, NAME_TYPE)
     CORE.gather_images()
 
-#Cute gathering
-CUTE = Redditscraping(CUTE_REDDITS, (IMAGE_PATH + "/cute/"), IMAGE_LIMIT, NAMES_TYPE)
-CUTE.gather_images()
+#scraping
+for name in FOLDER_NAMES:
+    try:
+        selected_folder = str(name)
+        selected_subreddits = str(CONFIG.get("scraping_options", selected_folder)).replace(" ", "").split(",")
+        scraping = Redditscraping(selected_subreddits, (IMAGE_PATH + "/%s/" % name), IMAGE_LIMIT, NAME_TYPE)
+        scraping.gather_images()
+    except Exception as error:
+        if type(error).__name__ == "NoOptionError":
+            print("folder_name: ''%s'' did not have any provided subreddits - code: 01" % name)
