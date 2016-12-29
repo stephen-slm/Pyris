90830c92fb590b1831777ec90e61a4ba9e425367
Stephen <smmstephenmiller@gmail.com>
2016-12-29 18:02:30 +0000

    default value file_type
    * file type is defulted to "jpeg" if not found

--- components/Redditscraping.py
+++ components/Redditscraping.py
@@ -62,7 +62,13 @@ class Redditscraping:
         print("Downloading image room: r/{}, {}/{}".format(room, str(self.i), str(self.image_count)))
         try:
             urllib.request.urlretrieve(url, name)
-            rename(name, "{file_name}.{file_format}".format(file_name=name, file_format=img_type(name)))
+            file_type = img_type(name)
+
+            if not file_type.lower() == "none":
+                rename(name, "{file_name}.{file_format}".format(file_name=name, file_format=file_type))
+            else:
+                rename(name, "{file_name}.{file_format}".format(file_name=name, file_format="jpeg"))
+
             self.i += 1
         except (urllib.request.HTTPError, Exception) as err:
             self.handle_errors(1, err, url)
